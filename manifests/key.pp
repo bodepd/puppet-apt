d7778840b34afb1280c9a6442839b06d9f2351d6
Edgar Magana <emagana@gmail.com>
2012-11-20 07:49:57 -0800

    Fix typo in key definition

--- manifests/key.pp
+++ manifests/key.pp
@@ -57,7 +57,7 @@ define apt::key (
         exec { $digest:
           command   => $digest_command,
           path      => '/bin:/usr/bin',
-          unless    => "/usr/bin/apt-key list | /bin/grep `/bin/echo ${upkey} | /bin/sed -e 's/.*(........)$/\\1/' `",
+          unless    => "/usr/bin/apt-key list | /bin/grep `/bin/echo ${upkey} | /bin/sed -e 's/.*\(........\)$/\\1/' `",
           logoutput => 'on_failure',
           before    => Anchor["apt::key ${upkey} present"],
         }
